b78ea781e6511eaca4710bdf46af9cc86be1be50
Himanshu Rawat <88080283+helsinfranz@users.noreply.github.com>
2025-06-22 19:29:26 +0530

    Update ipfsUpload.js

--- utils/ipfsUpload.js
+++ utils/ipfsUpload.js
@@ -3,8 +3,8 @@ export async function uploadToIPFS(data) {
     const response = await fetch("https://api.pinata.cloud/pinning/pinJSONToIPFS", {
       method: "POST",
       headers: {
-        pinata_api_key: process.env.PINATA_API_KEY,
-        pinata_secret_api_key: process.env.PINATA_SECRET_API_KEY,
+        pinata_api_key: process.env.NEXT_PUBLIC_PINATA_API_KEY,
+        pinata_secret_api_key: process.env.NEXT_PUBLIC_PINATA_SECRET_API_KEY,
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
